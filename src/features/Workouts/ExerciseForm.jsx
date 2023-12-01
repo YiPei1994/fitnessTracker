4ddb4dd95ad2dc926547ddd32b563462a13b657f
yipei <studentypz@gmail.com>
2023-12-01 15:46:52 +0100

    added form

--- src/features/Workouts/ExerciseForm.jsx
+++ src/features/Workouts/ExerciseForm.jsx
@@ -1,12 +1,40 @@
 import React from "react";
-import { Form } from "react-hook-form";
 import FormRow from "../ui/FormRow";
+import Input from "../ui/Input";
+import { Box, Button } from "@chakra-ui/react";
+import { AddIcon } from "@chakra-ui/icons";
+import FileInput from "../ui/FileInput";
 
 function ExerciseForm() {
   return (
-    <Form>
-      <FormRow></FormRow>
-    </Form>
+    <form>
+      <FormRow label="Name">
+        <Input type="text" placeholder="name of exercies" />
+      </FormRow>
+      <FormRow label="Type">
+        <Input type="text" placeholder="what type (push/pull/leg)" />
+      </FormRow>
+      <FormRow label="Sets">
+        <Input type="number" defaultValue={3} />
+      </FormRow>
+      <FormRow label="Reps">
+        <Input type="number" defaultValue={10} />
+      </FormRow>
+      <FormRow label="Break time">
+        <Input type="number" defaultValue={60} />
+      </FormRow>
+      <FormRow label="Instructions">
+        <Input type="text" placeholder="short instruction for the exercise" />
+      </FormRow>
+      <FormRow label="Image">
+        <FileInput id="image" accept="image/*" type="file" />
+      </FormRow>
+      <Box>
+        <Button>
+          <AddIcon />
+        </Button>
+      </Box>
+    </form>
   );
 }
 
